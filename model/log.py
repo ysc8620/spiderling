a65fb09bf64a9ea877f7f63040b81d534c00696f
ysc8620 <ysc8620@163.com>
2014-09-08 18:44:39 +0800

    ds

--- model/log.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'ShengYue'
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S', filename='system.log',filemode='a+')
