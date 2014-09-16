d8a1570b5a95cd0ef8ce0ba64132308482b0c017
ysc8620 <ysc8620@163.com>
2014-09-16 20:44:58 +0800

    open download img

--- scrapy-redis/example-project/main.py
+++ scrapy-redis/example-project/main.py
@@ -1,9 +1,13 @@
-import redis
-#import MySQLdb
-import scrapy.contrib.pipeline.images.ImagesPipeline
+#import redis
+import MySQLdb
+#import scrapy.contrib.pipeline.images.ImagesPipeline
 
 # pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
 # r = redis.Redis(connection_pool=pool)
 #
 # #r.set('name', 'hello')
 # print r.llen('dmoz:items')
+
+conn = MySQLdb.connect(user = 'root',db='test',passwd = '24abcdef',host='127.0.0.1')
+cursor = conn.cursor()
+cursor.execute('insert into links(url,md5url)values(%s, %s)', ('url','url'))
