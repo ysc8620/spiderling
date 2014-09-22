7eb785546cf0cb09df2bdc3949375deb6bb07314
ysc8620 <ysc8620@163.com>
2014-09-22 09:33:04 +0800

    goose file

---
+++ go.py
@@ -0,0 +1,7 @@
+#-*-coding:utf-8-*-
+from goose import Goose
+from goose.text import StopWordsChinese
+url  = 'http://news.cnblogs.com/n/504072/'
+g = Goose({'stopwords_class': StopWordsChinese})
+article = g.extract(url=url)
+print article.canonical_link
