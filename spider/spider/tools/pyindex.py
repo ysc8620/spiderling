d7ea62d1af8258f8a0d101daaa9159fa7f96937d
ysc8620 <ysc8620@163.com>
2014-09-24 22:40:48 +0800

    index

--- spider/spider/tools/pyindex.py
+++ spider/spider/tools/pyindex.py
@@ -76,8 +76,8 @@ mapping = {
 conn.indices.put_mapping("test-type", {'properties':mapping}, ["test-index"])
 
 
-conn.index({"name":"Joe Tester", "parsedtext":"Joe Testere nice guy", "uuid":"11111", "position":1}, "test-index", "test-type", 'sdfsdfsdfsdfs')
-conn.index({"name":"Bill Baloney", "parsedtext":"Joe Testere nice guy", "uuid":"22222", "position":2}, "test-index", "test-type",'sdfsdfsdfsfsfewrwsddfsdfs')
+conn.index({"name":"Joe Tester", "title":"sssss", "parsedtext":"Joe Testere nice guy", "uuid":"11111", "position":1}, "test-index", "test-type", 'sdfsdfsdfsdfs')
+conn.index({"name":"Bill Baloney",  "title":"ssssdddss","parsedtext":"Joe Testere nice guy", "uuid":"22222", "position":2}, "test-index", "test-type",'sdfsdfsdfsfsfewrwsddfsdfs')
 
 
 conn.indices.refresh("test-index") # Single index.
