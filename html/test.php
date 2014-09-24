d358688ad2187edb555aaca1ff59db0b930a1a41
ysc8620 <ysc8620@163.com>
2014-09-25 07:29:02 +0800

    php page

--- html/test.php
+++ html/test.php
@@ -2,11 +2,12 @@
 
 
 $conn = new Mongo("127.0.0.1");
-echo $conn->test->goods->find()->count();
+$db = $conn->test;
+$table = $db->goods;
 
 require 'vendor/autoload.php';
 
-$client = new Elasticsearch\Client(array("hosts"=>array("host"=>"127.0.0.1", "port"=>"9200")));
+$client = new Elasticsearch\Client(array("hosts"=>array("host"=>"54.255.39.86", "port"=>"9200")));
 
 #$query  = new Elasticsearch\Elastica\Query();
 
@@ -17,4 +18,11 @@ $searchParams['size'] = 12;
 #$searchParams['body']['query']['match']['title'] = 'iphone';
 $queryResponse = $client->search($searchParams);
 
-print_r($queryResponse);
+$data_list = array();
+if(is_array($queryResponse)){
+    foreach($queryResponse['hits']['hits'] as $item){
+        $row = $table->find(array('_id'=>$item['_id']));
+        print_r($row);
+    }
+}
+?>
