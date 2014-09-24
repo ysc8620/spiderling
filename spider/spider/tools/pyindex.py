915cac764276c42f1c62cf5bf1f89c78ba42bc34
ysc8620 <ysc8620@163.com>
2014-09-24 22:54:07 +0800

    index

--- spider/spider/tools/pyindex.py
+++ spider/spider/tools/pyindex.py
@@ -36,9 +36,12 @@ conn = ES('127.0.0.1:9200') # Use HTTP
 try:
     conn.indices.delete_index("test-index")
 except:
-    pass
+    try:
+        conn.indices.delete_index("goods-index")
+    except:
+        pass
 
-conn.indices.create_index("test-index")
+conn.indices.create_index("goods-index")
 
 mapping = {
     'title': {
@@ -73,16 +76,16 @@ mapping = {
         'type': 'string'
     }
 }
-conn.indices.put_mapping("test-type", {'properties':mapping}, ["test-index"])
+conn.indices.put_mapping("goods-type", {'properties':mapping}, ["goods-index"])
 
 con = Connection('localhost', 27017)
 db = con.test
 goods_list = db.goods.find()
 i = 1
 for goods in goods_list:
-    conn.index({"title":goods["title"], "brand":goods["brand"], "category":goods["category"], "price":goods["price"], "unique_id":goods["unique_id"]}, "test-index", "test-type", 'sdfsdfsdfsdfs')
+    conn.index({"title":goods["title"], "brand":goods["brand"], "category":goods["category"], "price":goods["price"], "unique_id":goods["unique_id"]}, "goods-index", "goods-type", goods["unique_id"])
     i = i + 1
-
+print i
 # conn.index({"name":"Joe Tester", "title":"sssss", "parsedtext":"Joe Testere nice guy", "uuid":"11111", "position":1}, "test-index", "test-type", 'sdfsdfsdfsdfs')
 # conn.index({"name":"Bill Baloney",  "title":"ssssdddss","parsedtext":"Joe Testere nice guy", "uuid":"22222", "position":2}, "test-index", "test-type",'sdfsdfsdfsfsfewrwsddfsdfs')
 
