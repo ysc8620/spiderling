e8864fd8d26c60e419ac796200e4a201a7e79f98
ysc8620 <ysc8620@163.com>
2014-09-24 22:14:51 +0800

    index

--- spider/spider/tools/pyindex.py
+++ spider/spider/tools/pyindex.py
@@ -11,13 +11,12 @@ except:
      pass
 conn.indices.create_index('godos-index') #
 mapping = {
-            u'title':{'boost': 1.0,'index': 'analyzed','store': 'yes','type': u'string',
-                      "term_vector" : "with_positions_offsets"},
+            u'title':{'boost': 1.0,'index': 'analyzed','store': 'yes','type': u'string',"term_vector" : "with_positions_offsets"},
             u'brand':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'string'},
             u'category':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'string'},
             u'price':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'float'},
             u'add_time':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'string'},
-            u'unique_id':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'string'}
+            #u'unique_id':{'boost': 1.0,'index': 'not_analyzed','store': 'yes','type': u'string'}
           }
 conn.indices.put_mapping("goods", {'properties':mapping}, ["godos-index"])
 con = Connection('localhost', 27017)
@@ -25,7 +24,7 @@ db = con.test
 goods_list = db.goods.find()
 i = 1
 for goods in goods_list:
-    conn.index({'title':goods['title'], 'brand':goods['brand'], 'category':goods['category'],'price':goods['price'],'add_time':goods['add_time'],'unique_id':goods['unique_id']}, 'godos-index', 'goods', i, True)
+    conn.index({'title':goods['title'], 'brand':goods['brand'], 'category':goods['category'],'price':goods['price'],'add_time':goods['add_time']}, 'godos-index', 'goods', i, True)
     i = i + 1
                  #向human的man中添加索引
 conn.refresh()
