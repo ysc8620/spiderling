a3b4418a0638080b8637f75070a20f9b871976dd
ysc8620 <ysc8620@163.com>
2015-02-05 09:15:20 +0800

    ds

--- spider/spider/tools/tools.py
+++ spider/spider/tools/tools.py
@@ -2,8 +2,24 @@
 __author__ = 'ShengYue'
 import re
 from pymongo import Connection
+import MySQLdb
 
 
+
+#conn = MySQLdb.connect(user = 'root',db='test',passwd = 'LEsc2008',host='localhost')
+#10.144.129.241
+conn = MySQLdb.connect(user = 'root',db='ilovedeals',passwd = 'ntucdbs911',host='10.144.129.241',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
+cursor = conn.cursor(cursorclass = MySQLdb . cursors . DictCursor)
+cursor.execute('SET NAMES utf8')
+res = cursor.execute("SELECT * FROM le_goods WHERE website_id=76 AND url=%s",['https://www.imsobshop.sg/fashion/men-s/burberry-london-men-edt-100ml'])
+print cursor.fetchone()
+conn.close()
+
+
+res = re.match(r".*?([\w\-]+)$", "http://www.baidu.com/sss/wer-3#ss")
+print res.group(1)
+
+exit()
 #rer = re.compile(r"(new-products|top-sellers|special-price).*?")
 res = re.match(r".*?/(new-products|top-sellers|special-price|faq|brands|payment-methods|howtoshop|about|affiliate)/", 'http://www.lazada.sg/special-price/?page=2')
 print res.group(1)
