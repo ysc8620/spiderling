df8e8e9aac02533a9d4c5f3722060ce3a3e12b1e
ysc8620 <ysc8620@163.com>
2015-03-05 15:44:33 +0800

    ds

--- spider/main.py
+++ spider/main.py
@@ -4,5 +4,6 @@ __author__ = 'Administrator'
 # http://news.cnblogs.com
 #from scrapy.contrib.pipeline.media import MediaPipeline
 import os
-os.chrdir(os.getcwd())
+
+os.chdir(os.getcwd())
 print os.system("scrapy crawl dmoz -a n=xxx")
