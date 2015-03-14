fa540b64ed393668976593612dbd12a5fb32966f
ysc8620 <ysc8620@163.com>
2015-03-15 07:19:31 +0800

    ds

--- spider_movie/main.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-__author__ = 'Administrator'
-
-# http://news.cnblogs.com
-#from scrapy.contrib.pipeline.media import MediaPipeline
-import os
-
-os.chdir(os.getcwd())
-print os.system("scrapy crawl dmoz -a n=xxx")
--- spider_movie/png.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-import os
-img = Image.open(os.getcwd()+"/1.png")
-img.save(os.getcwd()+"/.jpeg", format="jpeg")
