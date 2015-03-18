3d86b19e53bf483d814e55aae9d74864c938e595
ysc8620 <ysc8620@163.com>
2015-03-18 10:09:08 +0800

    ds

--- spider/spider/tools/db.py
+++ spider/spider/tools/db.py
@@ -15,29 +15,39 @@ class DB:
         self.connect()
 
     def connect(self):
-        self.conn = MySQLdb.connect(user='24a',db='ilovedeals',passwd='24abcdef',host='10.144.129.241',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
+        # self.conn = MySQLdb.connect(user='24a',db='ilovedeals',passwd='24abcdef',host='10.144.129.241',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
         # self.conn = MySQLdb.connect(user='24a',db='test',passwd='24abcdef',host='localhost',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
-        # self.conn = MySQLdb.connect(user = 'root',db='test',passwd = 'LEsc2008',host='localhost')
+        self.conn = MySQLdb.connect(user = 'root',db='test',passwd = 'LEsc2008',host='localhost')
 
     def execute(self, sql, args=None):
         try:
-            cursor = self.conn.cursor(cursorclass = MySQLdb . cursors . DictCursor)
-            cursor.execute('SET NAMES utf8')
+            self.cursor = self.conn.cursor(cursorclass = MySQLdb . cursors . DictCursor)
+            self.cursor.execute('SET NAMES utf8')
         except:
             self.connect()
-            cursor = self.conn.cursor(cursorclass = MySQLdb . cursors . DictCursor)
-            cursor.execute('SET NAMES utf8')
+            self.cursor = self.conn.cursor(cursorclass = MySQLdb . cursors . DictCursor)
+            self.cursor.execute('SET NAMES utf8')
 
         try:
             if args is not None:
-                cursor.execute(sql,args)
+                self.cursor.execute(sql,args)
             else:
-                cursor.execute(sql)
+                self.cursor.execute(sql)
             self.conn.commit()
         except (AttributeError, MySQLdb.OperationalError):
             print 'Mysql execute error'
             logs('------ '+ time.strftime("%Y-%m-%d %H-%M-%S")+' Mysql execute error: '+sql)
-        return cursor
+        return self.cursor
+
+    def __del__(self):
+        try:
+            self.cursor.close()
+        except:
+            pass
+        try:
+            self.conn.close()
+        except:
+            pass
 
     def close(self):
         if(self.cursor):
