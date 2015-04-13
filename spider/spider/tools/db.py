369b7dfbc9c4c81bca576021564cbe2a8d0c1739
ysc8620 <ysc8620@163.com>
2015-04-13 14:50:54 +0800

    ds

--- spider/spider/tools/db.py
+++ spider/spider/tools/db.py
@@ -17,13 +17,13 @@ class DB:
         self.db = db
 
     def connect(self):
-        # if self.db == 'sg':
-        #     self.conn = MySQLdb.connect(user='24a',db='ilovedeals',passwd='24abcdef',host='10.144.129.241',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
-        # elif self.db == 'my':
-        #     self.conn = MySQLdb.connect(user='24a',db='myilovedeals',passwd='24abcdef',host='localhost',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
+        if self.db == 'sg':
+            self.conn = MySQLdb.connect(user='24a',db='ilovedeals',passwd='24abcdef',host='10.144.129.241',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
+        elif self.db == 'my':
+            self.conn = MySQLdb.connect(user='24a',db='myilovedeals',passwd='24abcdef',host='localhost',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
 
         #self.conn = MySQLdb.connect(user='24a',db='test',passwd='24abcdef',host='localhost',unix_socket='/tmp/mysql.sock')#,unix_socket='/tmp/mysql.sock'
-        self.conn = MySQLdb.connect(user = 'root',db='test',passwd = 'LEsc2008',host='localhost')
+        # self.conn = MySQLdb.connect(user = 'root',db='test',passwd = 'LEsc2008',host='localhost')
 
     def execute(self, sql, args=None):
         try:
