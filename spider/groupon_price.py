7edb7d4c3ee341cf5b3a1eec2345caaa31350cde
ysc8620 <ysc8620@163.com>
2016-06-04 06:15:47 +0800

    ds

--- spider/groupon_price.py
+++ spider/groupon_price.py
@@ -11,6 +11,7 @@ website_id=5
 db = DB('sg')
 Classifier = SimpleClassifier('sg')
 
+
 while True:
     r = requests.session()
     res = db.execute("SELECT goods_id,url FROM le_goods WHERE website_id=5 AND price=0 AND seller_user_id=0")
@@ -26,13 +27,21 @@ while True:
     price = 0
     print row['goods_id']
     if reprice:
-        price = reprice[0].replace('S$','').replace(',','')
+        try:
+            price =   float(reprice[0].replace('S$','').replace(',',''))
+        except:
+            price = 0
         print price,'-'
 
     price_old = 0
     if reprice_old:
-        price_old = reprice_old[0].replace('S$','').replace(',','')
+        try:
+            price_old = float(reprice_old[0].replace('S$','').replace(',',''))
+        except:
+            price_old = 0
         print price_old,'='
+
+    price_old = price_old + price
 
     db.execute("UPDATE le_goods SET seller_user_id=1,original_price=%s,price=%s WHERE goods_id=%s",[price_old,price,row['goods_id']])
     time.sleep(10)
